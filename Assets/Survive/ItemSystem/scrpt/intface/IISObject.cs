023ff9ba5a23ff879090376a3c7ae08c3ff0abb8
dedlift <realsquidofsteel@gmail.com>
2015-09-27 12:39:00 -0400

    no message

---
+++ Assets/Survive/ItemSystem/scrpt/intface/IISObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IISObject {
+    //name
+    //icon
+    string ISName { get; set; }
+    Sprite ISIcon { get; set; }
+
+    //value in gold
+    int ISValue { get; set; }
+    //quality
+    //durability
+    //size
+    //weight
+    int ISWeight { get; set; }
+
+    //canEquip
+    //isQuestItem
+    //canTakeDamage
+    //canTrade
+}
---
+++ Assets/Survive/ItemSystem/scrpt/intface/IISQuality.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IISQuality {
+    //grey white green blue orange teal purple
+    string Name { get; set; }
+    Sprite Icon { get; set; }
+}
